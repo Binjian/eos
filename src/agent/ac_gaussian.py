8c960a597f0950a8771b84ecf5b492fd6a9dcd40
binjian.xin <binjian.xin@newrizon.net>
2021-04-30 15:11:18 +0800

    add train_step to simpify main train loop code

--- src/agent/ac_gaussian.py
+++ src/agent/ac_gaussian.py
@@ -5,11 +5,43 @@ from tensorflow.keras import layers
 import tensorflow.keras.initializers as initializers
 import tensorflow_probability as tfp
 
+""" one training step to update network"""
+def train_step(net, history, optimizer, tape):
+    huber_loss = keras.losses.Huber()
+    actor_losses = []
+    critic_losses = []
+    for action, mu_sigma, value, ret in history:
+        # at this point in history, the critic estimated that we would get a
+        # total reward = `value` in the future. we took an action with log probability
+        # of `log_prob` and ended up recieving a total reward = `ret`.
+        # the actor must be updated so that it predicts an action that leads to
+        # high rewards (compared to critic's estimate) with high probability.
+        diff = ret - value
+        actor_loss = customlossgaussian(mu_sigma, action, diff)
+        actor_losses.append(actor_loss)  # actor loss
+
+        # the critic must be updated so that it predicts a better estimate of
+        # the future rewards.
+        # todo calculate loss_critic
+        critic_losses.append(
+            huber_loss(tf.expand_dims(value, 0), tf.expand_dims(ret, 0))
+        )
+
+    # now the agent backpropagate every episode. todo or backpropagation every n (say 20) episodes
+    # backpropagation
+    loss_value = sum(actor_losses) + 300 * sum(critic_losses)  # todo 300-400 times
+
+    grads = tape.gradient(loss_value, net.trainable_variables)
+    optimizer.apply_gradients(
+        zip(grads, net.trainable_variables)
+    )
+    return loss_value
+
+
+
 """construct the actor network with mu and sigma as output"""
-
-
 def constructactorcriticnetwork(
-    num_observations, sequence_len, num_actions, num_hidden, bias_mu, bias_sigma
+        num_observations, sequence_len, num_actions, num_hidden, bias_mu, bias_sigma
 ):
     inputs = layers.Input(
         shape=(sequence_len, num_observations)  # DONE should be flattened
