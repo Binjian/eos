768cae02841560326e098886e04c01b25e724d9d
binjian xin <binjian.xin@newrizon.net>
2021-05-08 20:36:30 +0800

    add logs and visualization with tensorboard; add a threading for visualization of plot

--- src/l045a_ac_tf.py
+++ src/l045a_ac_tf.py
@@ -36,7 +36,8 @@ as energy consumption
 """
 ## Setup
 """
-import os
+
+# drl import
 import datetime
 import gym
 import gym_carla
@@ -48,16 +49,25 @@ import tensorflow_probability as tfp
 
 tfd = tfp.distributions
 
+# communcation import
 import rospy
 import std_msgs.msg
 from comm.vcu.msg import *
 from threading import Lock
 import _thread as thread, queue, time
 
-from comm.udp_sender import (
-    send_table,
-    prepare_vcu_calibration_table,
-)
+# visualization import
+import pandas as pd
+import matplotlib.pyplot as plt
+from visualization.visual import plot_to_image
+
+from matplotlib import cm
+from matplotlib.ticker import LinearLocator, FormatStrFormatter
+from mpl_toolkits.mplot3d import Axes3D
+import seaborn as sns
+import io  # needed by convert figure to png in memory
+
+# internal import
 from comm.vcu_calib_generator import (
     generate_vcu_calibration,
     generate_lookup_table,
@@ -66,29 +76,74 @@ from comm.vcu_calib_generator import (
 
 # from communication import carla_ros
 from comm.carla_ros import talker
-from agent.ac_gaussian import customlossgaussian, constructactorcriticnetwork, train_step
+from agent.ac_gaussian import (
+    constructactorcriticnetwork,
+    train_step,
+)
 
 from comm.tbox.scripts.tbox_sim import *
 
-set_tbox_sim_path("/home/is/devel/carla-drl/drl-carla-manual/src/comm/tbox")
+set_tbox_sim_path("/home/is/devel/newrizon/drl-carla-manual/src/comm/tbox")
 # value = [99.0] * 21 * 17
 # send_float_array('TQD_trqTrqSetECO_MAP_v', value)
 
 # TODO add vehicle communication insterface
-# TODO add visualization and logging
 # TODO add model checkpoint episodically unique
 
-# dataqueue contains a table which is a list of type float
-# this is the consumer
+
+# multithreading initialization
+data_lock = Lock()
+vcu_output = VCU_Output()
+vcu_input = VCU_Input()
+# flash_mutex = thread.allocate_lock()
+
+# TODO add visualization and logging
+current_time = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+train_log_dir = "logs/gradient_tape/" + current_time + "/train"
+train_summary_writer = tf.summary.create_file_writer(train_log_dir)
+
+# dataQueue contains a table which is a list of type float
+dataQueue = queue.Queue()
+# figQueue is for visualization thread to show the calibration 3d figure
+figQueue = queue.Queue()
+episode_done = False
+episode_count = 0
+
+
+# this is the figure consumer for visualization
+def show_calib_table(figqueue):
+    while True:
+        time.sleep(0.1)
+        try:
+            figure = figqueue.get()  # default block = True
+        except queue.Empty:
+            pass
+        else:
+            plt.show()
+            time.sleep(1)
+            if episode_done:
+                with train_summary_writer.as_default():
+                    tf.summary.image(
+                        "Calibration Table", plot_to_image(figure), episode_count
+                    )
+            else:
+                plt.close(figure)
+            # print("show the calib table!")
+            # send_float_array('TQD_trqTrqSetECO_MAP_v', table)
+
+
+# flash_mutex.acquire()
+# this is the calibration table consumer for flashing
 def flash_vcu(dataqueue):
     while True:
         time.sleep(0.1)
         try:
-            table = dataqueue.get() # default block = True
+            table = dataqueue.get()  # default block = True
         except queue.Empty:
             pass
         else:
-            send_float_array('TQD_trqTrqSetECO_MAP_v', table)
+            print("flash vcu calib table!")
+            # send_float_array('TQD_trqTrqSetECO_MAP_v', table)
 
 
 def get_torque(data):
@@ -96,25 +151,17 @@ def get_torque(data):
     with data_lock:
         vcu_output.header = data.header
         vcu_output.torque = data.torque
+
+
 # TODO add a thread for send_float_array
 # TODO add printing calibration table
 # TODO add initialize table to EP input
 def main():
-
-    # multithreading initialization
-    data_lock = Lock()
-    vcu_output = VCU_Output()
-    vcu_input = VCU_Input()
-    # flash_mutex = thread.allocate_lock()
-    dataQueue = queue.Queue()
-    # flash_mutex.acquire()
-
+    global episode_done, episode_count
     # ros msgs for vcu communication
     rospy.init_node("carla", anonymous=True)
     rospy.Subscriber("/newrizon/vcu_output", VCU_Output, get_torque)
     pub = rospy.Publisher("/newrizon/vcu_input", VCU_Input, queue_size=10)
-
-
 
     # parameters for the gym_carla environment
     params = {
@@ -129,7 +176,7 @@ def main():
         "continuous_accel_range": [-3.0, 3.0],  # continuous acceleration range
         "continuous_steer_range": [-0.3, 0.3],  # continuous steering angle range
         "ego_vehicle_filter": "vehicle.carlamotors.carlacola",  # filter for defining ego vehicle
-        "carlaserver": "localhost",  # carla server ip address
+        "carlaserver": "192.168.8.100",  # carla server ip address
         "port": 2000,  # connection port
         "town": "town01",  # which town to simulate
         "task_mode": "random",  # mode of the task, [random, roundabout (only for town03)]
@@ -153,8 +200,7 @@ def main():
     seed = 42
     gamma = 0.99  # discount factor for past rewards
     max_steps_per_episode = 10000  # maximal steps per episode, 120s / 0.05 is 2400. todo adjust numbers of maximal steps
-    # env = gym.make("cartpole-v0")  # create the environment
-    # set gym-carla environment
+
     env = gym.make("carla-v0", params=params)
     obs = env.reset()
 
@@ -175,7 +221,9 @@ def main():
     """
     vcu_calib_table_col = 17  # number of pedal steps, x direction
     vcu_calib_table_row = 21  # numnber of velocity steps, y direction
-    vcu_calib_table_budget = 0.05  # interval that allows modifying the calibration table
+    vcu_calib_table_budget = (
+        0.05  # interval that allows modifying the calibration table
+    )
     vcu_calib_table_size = vcu_calib_table_row * vcu_calib_table_col
     pedal_range = [0, 1.0]
     velocity_range = [0, 20.0]
@@ -189,6 +237,7 @@ def main():
 
     # Start thread for flashing vcu, flash first
     thread.start_new_thread(flash_vcu, (dataQueue,))
+    thread.start_new_thread(show_calib_table, (figQueue,))
     # flash_mutex.acquire()
 
     vcu_lookup_table = generate_lookup_table(
@@ -217,8 +266,10 @@ def main():
     )
     opt = keras.optimizers.Adam(learning_rate=0.001)
     # add checkpoints manager
-    checkpoint_dir = '../tf_ckpts'
-    ckpt = tf.train.Checkpoint(step=tf.Variable(1), optimizer=opt, net=actorcritic_network)
+    checkpoint_dir = "../tf_ckpts"
+    ckpt = tf.train.Checkpoint(
+        step=tf.Variable(1), optimizer=opt, net=actorcritic_network
+    )
     manager = tf.train.CheckpointManager(ckpt, checkpoint_dir, max_to_keep=10)
     ckpt.restore(manager.latest_checkpoint)
     if manager.latest_checkpoint:
@@ -238,7 +289,7 @@ def main():
     vcu_rewards_history = []
     vcu_states_history = []
     running_reward = 0
-    episode_count = 0
+    episode_reward = 0
 
     wait_for_reset = True
     L045B = True
@@ -253,7 +304,6 @@ def main():
                 continue
             else:
                 wait_for_reset = False
-        episode_reward = 0
         vcu_reward = 0
         vcu_states = []
         vcu_states.append(obs)
@@ -261,10 +311,17 @@ def main():
             for timestep in range(1, max_steps_per_episode):
                 if L045B:
                     # while vcu_input.stamp > vcu_output.stamp:
-                    talker(pub, env.counter, obs[0], obs[1], obs[2]) # ... vel, acc, pedal
+                    talker(
+                        pub, env.counter, obs[0], obs[1], obs[2]
+                    )  # ... vel, acc, pedal
                     with data_lock:
                         throttle = vcu_output.torque
                         h1 = vcu_output.header
+
+                        # the following 3 lines need to be deleted
+                        throttle = vcu_lookup_table(
+                            obs[2], obs[0]
+                        )  # look up vcu table with pedal and speed  for throttle request
                 else:
                     throttle = vcu_lookup_table(
                         obs[2], obs[0]
@@ -273,7 +330,9 @@ def main():
                 action = [throttle, 0]
                 # print("action:{}".format(action[0]))
                 # print("env.throttle:{}".format(env.pedal))
+                # TODO l045a define episode done (time, distance, defined end event)
                 obs, r, done, info = env.step(action)
+                episode_done = done
                 vcu_reward += r
                 vcu_states.append(obs)
 
@@ -302,7 +361,7 @@ def main():
                     vcu_action_history.append(vcu_action)
                     # Here the loopup table with contrained output is part of the environemnt,
                     # clip is part of the environment to be learned
-                    # action is not contrained!
+                    # action is not constrained!
                     vcu_calib_table = tf.reshape(
                         vcu_action, [vcu_calib_table_row, vcu_calib_table_col]
                     )
@@ -313,6 +372,29 @@ def main():
                         clip_value_max=1.0,
                     )
 
+                    # Create a matplotlib 3d figure, //export and save in log
+                    pd_data = pd.DataFrame(
+                        vcu_calib_table.numpy(),
+                        columns=np.linspace(0, 1.0, num=17),
+                        index=np.linspace(0, 30, num=21),
+                    )
+                    df = pd_data.unstack().reset_index()
+                    df.columns = ["pedal", "velocity", "throttle"]
+
+                    fig = plt.figure()
+                    ax = fig.gca(projection="3d")
+                    surf = ax.plot_trisurf(
+                        df["pedal"],
+                        df["velocity"],
+                        df["throttle"],
+                        cmap=plt.cm.viridis,
+                        linewidth=0.2,
+                    )
+                    fig.colorbar(surf, shrink=0.5, aspect=5)
+                    ax.view_init(30, 135)
+                    # throw figure to the visualization thread
+                    figQueue.put(fig)
+                    # plt.show()
 
                     # action = np.random.choice(num_actions, p=np.squeeze(action_probs))
                     # action_probs_history.append(tf.math.log(action_probs[0, action]))
@@ -323,6 +405,7 @@ def main():
                         # with flash_mutex:
                         vcu_act_list = vcu_calib_table.numpy().reshape(-1).tolist()
                         dataQueue.put(vcu_act_list)
+                        print("update vcu calib table")
                         # send_float_array('TQD_trqTrqSetECO_MAP_v', vcu_calib_table.numpy().reshape(-1).tolist() )
                     else:
                         vcu_lookup_table = generate_lookup_table(
@@ -363,16 +446,46 @@ def main():
             )
 
             # back propagation
-            loss = train_step(actorcritic_network, history, opt, tape)
+            (
+                loss_all,
+                act_losses_all,
+                entropy_losses_all,
+                critic_losses_all,
+            ) = train_step(actorcritic_network, history, opt, tape)
             ckpt.step.assign_add(1)
 
-            # clear the loss and reward history
-            vcu_states_history.clear()
-            vcu_action_history.clear()
-            vcu_rewards_history.clear()
-            mu_sigma_history.clear()
-            vcu_critic_value_history.clear()
-            obs = env.reset()
+        with train_summary_writer.as_default():
+            tf.summary.scalar("loss_sum", loss_all, step=episode_count)
+            tf.summary.scalar("loss_act", act_losses_all, step=episode_count)
+            tf.summary.scalar("loss_entropy", entropy_losses_all, step=episode_count)
+            tf.summary.scalar("loss_critic", critic_losses_all, step=episode_count)
+            tf.summary.scalar("episode reward", episode_reward, step=episode_count)
+
+        output_template = "Episode {}, Loss all: {}, Act loss: {}, Entropy loss: {}, Critic loss: {}, Episode Reward: {}"
+        print(
+            output_template.format(
+                episode_count + 1,
+                loss_all,
+                act_losses_all,
+                entropy_losses_all,
+                critic_losses_all,
+                episode_reward,
+            )
+        )
+        #
+        #   # Reset metrics every epoch
+        #   train_loss.reset_states()
+        #   test_loss.reset_states()
+        #   train_accuracy.reset_states()
+        #   test_accuracy.reset_states()
+        #   clear the loss and reward history
+        vcu_states_history.clear()
+        vcu_action_history.clear()
+        vcu_rewards_history.clear()
+        mu_sigma_history.clear()
+        vcu_critic_value_history.clear()
+        obs = env.reset()
+        episode_reward = 0
         # log details
         # actorcritic_network.save_weights("./checkpoints/cp-{epoch:04d}.ckpt")
         # actorcritic_network.save("./checkpoints/cp-last.kpt")
@@ -383,12 +496,12 @@ def main():
         # Checkpoint manager save model
         save_path = manager.save()
         print("Saved checkpoint for step {}: {}".format(int(ckpt.step), save_path))
-        print("loss {:1.2f}".format(loss.numpy()))
+        print("loss {:1.2f}".format(loss_all.numpy()))
 
         episode_count += 1
         if episode_count % 1 == 0:
             template = "running reward: {:.2f} at episode {}"
-            print('========================')
+            print("========================")
             print(template.format(running_reward, episode_count))
 
         # TODO terminate condition to be defined: reward > limit (percentage); time too long
