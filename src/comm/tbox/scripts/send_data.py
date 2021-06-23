7b878fe79c964e144f1e2385bc26a9f85ef53594
binjian.xin <binjian.xin@newrizon.net>
2021-06-23 19:26:31 +0800

    remove tbox, preparing for add submodule tbox-simulator

--- src/comm/tbox/scripts/send_data.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-
-import tbox_sim
-
-if __name__ == "__main__":
-    # args = parse_arg()
-    value = [99.0] * 21 * 17
-    tbox_sim.set_tbox_sim_path("/home/is/devel/newrizon/drl-carla-manual/src/comm/tbox")
-    tbox_sim.send_float_array("TQD_trqTrqSetECO_MAP_v", value)
