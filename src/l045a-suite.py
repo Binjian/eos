bb7a69a652d9d78b43719dcb95f9cb09fbe99ed8
binjian.xin <binjian.xin@newrizon.net>
2021-06-30 15:11:10 +0800

    add udp recording script with multiprocesses

--- src/l045a-suite.py
+++ src/l045a-suite.py
@@ -1,16 +1,23 @@
 import os
 import time
 
-fileName = '../data/udp-pcap/data.pcap' # file name
-portNum = 8002 # port number
-sudoPassword = 'asdf' # system password
-command = 'tcpdump udp -w '+ fileName +' -i lo port ' + str(portNum) # system command
+import os
+import time
+import signal
+import subprocess
 
+fileName = "../data/udp-pcap/data.pcap"  # file name
+portNum = 8002  # port number
 pid = os.fork()
 if pid == 0:  # copy process
     time.sleep(1)
-    os.system('echo %s|sudo -S %s' % (sudoPassword, command))  # catch packets via tcpdump
-    assert False, "error starting program"  # shouldn't return
-else:
-    print("Child is", pid)    
     os.execlp("python", "python", "l045a_ac_tf.py")  #  run Simulation
+else:
+    p = subprocess.Popen(
+        ["tcpdump", "udp", "-w", fileName, "-i", "lo", "port", str(portNum)],
+        stdout=subprocess.PIPE,
+    )
+    result = os.waitpid(-1, 0)
+    p.terminate()
+
+
