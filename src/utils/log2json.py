64b6215f808e9464d7d0165bb5bb91e57552b33d
hongchen.pan <hongchen.pan@newrizon.com>
2021-12-08 14:41:56 +0800

    add argument parser to log2json

--- src/utils/log2json.py
+++ src/utils/log2json.py
@@ -1,9 +1,37 @@
 import json
+import argparse
+import os
+# argument parser
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    "-f",
+    "--file",
+    type=str,
+    help="log file path, the file to be converted to json format",
+)
+parser.add_argument(
+    "-s",
+    "--scratch",
+    help="log file is in scratc/py_logs or just py_logs",
+    action="store_true",
+)
+args = parser.parse_args()
 
-# log filename
-logfolder = "../../data/scratch/py_logs/"
-logfilename = "l045a_ddpg-episodefree-21-11-29-11-52-35"
-logfile = logfolder + logfilename + ".log"
+# log folder
+if args.scratch:
+    logfolder = "../../data/scratch/py_logs/"
+else:
+    logfolder = "../../data/py_logs/"
+
+# file name
+if args.file:    
+    logfilename = args.file
+    logfile = logfolder + logfilename
+else:
+    # default file name
+    logfilename = sorted([f for f in os.listdir(logfolder)])[-1]
+    print(logfilename)
+    logfile = os.path.join(logfolder,logfilename)
 # in log file
 with open(logfile) as f:
     # line by line
@@ -16,7 +44,10 @@ with open(logfile) as f:
         l = json.loads(l)
         # append dict
         result.append(l)
-# write to json
+
+# get log file name
+jsonFileName = logfilename.split(".")[0] + "\n"
+# write to json file
 jsonFile = open("../../data/scratch/json_logs/" + logfilename + ".json", "w")
 jsonString = json.dumps(result)
 jsonFile.write(jsonString)
