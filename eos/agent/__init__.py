41bb46cfeaf0eb53d0bd55ed7a412b8583043e86
binjian.xin <binjian.xin@newrizon.com>
2022-05-11 22:29:37 +0800

    [add] __all__

--- eos/agent/__init__.py
+++ eos/agent/__init__.py
@@ -13,3 +13,17 @@ from .ddpg import (
     get_actor,
     policy,
 )
+
+__all__ = [
+    RDPG,
+    OUActionNoise,
+    train_step_a2c,
+    train_step_ddpg,
+    constructactorcriticnetwork_a2c,
+    customlossgaussian_a2c,
+    Buffer,
+    update_target,
+    get_critic,
+    get_actor,
+    policy,
+]
