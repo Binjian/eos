a071e285f38a3b0f4ccb98991c78d42ccecf3e7a
binjian.xin <binjian.xin@newrizon.com>
2022-05-12 15:45:24 +0800

    [add] package script entry point

--- eos/__main__.py
+++ eos/__main__.py
@@ -1,20 +1,21 @@
-import sys, os
+import os
 import pprint
+from eos import logger, projroot
 
-from .agent import *
-
-# from src import PROJROOT
-from .comm import *
-from .utils import *
+from eos.realtime_train_infer_ddpg import realtime_train_infer_ddpg
+pp = pprint.PrettyPrinter(indent=40)
 
 
 # Set the project root directory
 # Set the package directory
 def main():
-    pp = pprint.PrettyPrinter(indent=40)
     pp.pprint(f"veos __main__ CWD: {os.getcwd()}")
+    app = realtime_train_infer_ddpg(
+        False, False, True, 'install', projroot, logger
+    )
+    app.run()
 
 
 if __name__ == "__main__":
-    print("test of veos")
+    pp.pprint("test of realtime_train_infer_ddpg")
     main()
