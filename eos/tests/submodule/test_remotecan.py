122ac806525588ef746232a21a4d5491f893448f
binjian.xin <binjian.xin@newrizon.com>
2022-05-19 10:55:43 +0800

    [add] numpy ragged array handling
    Unittest setUp method
    numpy warning handling
    numpy printoption

--- eos/tests/submodule/test_remotecan.py
+++ eos/tests/submodule/test_remotecan.py
@@ -13,34 +13,39 @@ from eos import remote_can_client
 
 # import ...src.comm.remotecan.remote_can_client.remote_can_client
 
+# ignore DeprecationWarning
+warnings.filterwarnings("ignore", message='currentThread',category=DeprecationWarning)
+np.warnings.filterwarnings('ignore', category=DeprecationWarning)
 
 class TestRemoteCan(unittest.TestCase):
     """Tests for 'remote_can_client.py'."""
-
     site = "internal"
-
-    @unittest.skipIf(site == "internal", "skip for internal test")
-    def test_proxy(self):
-        proxies = {
+    def setUp(self) -> None:
+        """Set up proxy and client"""
+        self.proxies = {
             "http": "http://127.0.0.1:20171",
             "https": "http://127.0.0.1:20171",
         }
-        proxies_socks = {
+        self.proxies_socks = {
             "http": "socks5://127.0.0.1:20170",
             "https": "socks5://127.0.0.1:20170",
         }
-        proxies_lantern = {
+        self.proxies_lantern = {
             "http": "http://127.0.0.1:34663",
             "https": "http://127.0.0.1:34663",
         }
-        client = remote_can_client.RemoteCan(
-            vin="987654321654321M4", proxies=proxies_lantern
-        )
+        os.environ["http_proxy"] = ""  # for native test (internal site force no proxy)
+        self.client = remote_can_client.RemoteCan(vin="987654321654321M4")
 
+    @unittest.skipIf(site == "internal", "skip for internal test")
+    def test_proxy(self):
+        self.client = remote_can_client.RemoteCan(
+            vin="987654321654321M4", proxies=self.proxies_lantern
+        )
         map2d = [[i * 10 + j for j in range(17)] for i in range(21)]
-        success, reson = client.send_torque_map(map2d)
+        success, reson = self.client.send_torque_map(map2d)
         if success:
-            signal_success, json_ret = client.get_signals(duration=2)
+            signal_success, json_ret = self.client.get_signals(duration=2)
             if signal_success is True:
                 try:
                     print("print whole json string:")
@@ -58,12 +63,10 @@ class TestRemoteCan(unittest.TestCase):
             print("reson", reson)
 
     def test_native(self):
-        client = remote_can_client.RemoteCan(vin="987654321654321M4")
-        os.environ["http_proxy"] = ""  # for native test (internal site force no proxy)
         map2d = [[i * 10 + j for j in range(17)] for i in range(21)]
-        success, reson = client.send_torque_map(map2d)
+        success, reson = self.client.send_torque_map(map2d)
         if success:
-            signal_success, json_ret = client.get_signals(duration=2)
+            signal_success, json_ret = self.client.get_signals(duration=2)
             if signal_success is True:
                 try:
                     # json_string = json.dumps(
@@ -73,32 +76,61 @@ class TestRemoteCan(unittest.TestCase):
 
                     for key, value in json_ret.items():
                         if key == "result":
+                            # with np.printoptions(precision=4, suppress=True, formatter={'float': '{:0.1f}'.format}, linewidth=100):
+                            with np.printoptions(suppress=True, linewidth=100):
+                                # capture warning about ragged json arrays
+                                with np.testing.suppress_warnings() as sup:
+                                    log_warning = sup.record(np.VisibleDeprecationWarning, "Creating an ndarray from ragged nested sequences")
+                                    current = np.array(value["list_current_1s"])
+                                    if len(log_warning) > 0:
+                                        log_warning.pop()
+                                        item_len = [len(item) for item in current]
+                                        for count, item in enumerate(current):
+                                            item[item_len[count] : max(item_len)] = None
+                                    print(f"current{current.shape}:{current}")
 
-                            with warnings.catch_warnings(record=True) as w:
-                                warnings.simplefilter("always")
-                                current = np.array(value["list_current_1s_zoomed"])
-                                if len(w) > 0:
-                                    item_len = [len(item) for item in current]
-                                    for count, item in enumerate(current):
-                                        item[item_len[count] : item_len.max()] = None
+                                    voltage = np.array(value["list_voltage_1s"])
+                                    if len(log_warning):
+                                        log_warning.pop()
+                                        item_len = [len(item) for item in voltage]
+                                        for count, item in enumerate(voltage):
+                                            item[item_len[count] : max(item_len)] = None
+                                    print(f"voltage{voltage.shape}:{voltage}")
 
-                            print(f"current{current.shape}:{current}")
-                            voltage = np.array(value["list_voltage_1s"])
-                            print(f"voltage{voltage.shape}:{voltage}")
-                            thrust = np.array(value["list_pedal_1s"])
-                            print(f"accl{thrust.shape}:{thrust}")
-                            brake = np.array(value["list_brake_pressure_1s"])
-                            print(f"brake{brake.shape}:{brake}")
-                            velocity = np.array(value["list_speed_1s"])
-                            print(f"velocity{velocity.shape}:{velocity}")
-                            gears = np.array(value["list_gears"])
-                            print(f"gears{gears.shape}:{gears}")
-                            delay = np.array(value["signal_dalay"])
-                            print(f"delay{delay.shape}:{delay}")
-                            timestamp = np.array(value["timestamp"])
-                            print(
-                                f"timestamp{timestamp.shape}:{datetime.fromtimestamp(timestamp.tolist())}"
-                            )
+                                    thrust = np.array(value["list_pedal_1s"])
+                                    if len(log_warning) > 0:
+                                        log_warning.pop()
+                                        item_len = [len(item) for item in thrust]
+                                        for count, item in enumerate(thrust):
+                                            item[item_len[count] : max(item_len)] = None
+                                    print(f"accl{thrust.shape}:{thrust}")
+
+                                    brake = np.array(value["list_brake_pressure_1s"])
+                                    if len(log_warning) > 0:
+                                        log_warning.pop()
+                                        item_len = [len(item) for item in brake]
+                                        for count, item in enumerate(brake):
+                                            item[item_len[count] : max(item_len)] = None
+                                    print(f"brake{brake.shape}:{brake}")
+
+                                    velocity = np.array(value["list_speed_1s"])
+                                    if len(log_warning) > 0:
+                                        log_warning.pop()
+                                        item_len = [len(item) for item in velocity]
+                                        for count, item in enumerate(velocity):
+                                            item[item_len[count] : max(item_len)] = None
+                                    print(f"velocity{velocity.shape}:{velocity}")
+
+                                    gears = np.array(value["list_gears"])
+                                    if len(log_warning) > 0:
+                                        log_warning.pop()
+                                        item_len = [len(item) for item in gears]
+                                        for count, item in enumerate(gears):
+                                            item[item_len[count] : max(item_len)] = None
+                                    print(f"gears{gears.shape}:{gears}")
+
+                                    timestamp = np.array(value["timestamp"])
+                                    print(f"timestamp{timestamp.shape}:{datetime.fromtimestamp(timestamp.tolist())}")
                         else:
                             print(f"{key}:{value}")
                 except Exception as X:
