8e2081895b1843a86258319801134008e05f2bb0
binjian.xin <binjian.xin@newrizon.com>
2022-05-26 11:52:32 +0800

    [refactor]  move plot from utils to visualization

--- eos/utils/__init__.py
+++ eos/utils/__init__.py
@@ -1,4 +1,3 @@
 from .log import get_logger
-from .plot import plot_3d_figure
 
-__all__ = ["get_logger", "plot_3d_figure"]
+__all__ = ["get_logger"]
--- eos/visualization/__init__.py
+++ eos/visualization/__init__.py
@@ -1,2 +1,4 @@
 from .visual import plot_to_image, visual, compare_pic, gen_report
-__all__ = ['plot_to_image', 'visual', 'compare_pic', 'gen_report']
+from .plot import plot_3d_figure
+
+__all__ = ['plot_to_image', 'visual', 'compare_pic', 'gen_report', 'plot_3d_figure']
