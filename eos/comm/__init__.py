6b86eb353a537c07aed224033e3d76ce9a62d599
Binjian Xin <binjian.xin@newrizon.net>
2022-06-08 19:02:35 +0800

    [fix] submodule path setting

--- eos/comm/__init__.py
+++ eos/comm/__init__.py
@@ -1,8 +1,9 @@
 from .remotecan.remote_can_client.remote_can_client import RemoteCan
 from .tbox.scripts.tbox_sim import set_tbox_sim_path, send_float_array
 from .vcu_calib_generator import generate_vcu_calibration, generate_lookup_table
+from pathlib import Path
 
-set_tbox_sim_path("./tbox")
+set_tbox_sim_path(str(Path(__file__).parent) +"/tbox")
 kvaser_send_float_array = send_float_array
 
 
