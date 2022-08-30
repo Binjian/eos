f47607716e89a9e2303f1e0499228beff039e742
binjian.xin <binjian.xin@newrizon.com>
2022-08-30 08:34:25 +0800

    [add] episode collection

--- eos/tests/submodule/test_remotecan_pool.py
+++ eos/tests/submodule/test_remotecan_pool.py
@@ -60,7 +60,8 @@ class TestRemoteCanPool(unittest.TestCase):
         self.trucks = trucks
         self.truck_name = "VB7"
 
-        self.schema = []
+        self.rec_schema = []
+        self.epi_schema = []
         self.record = []
         self.projroot = projroot
         self.logger = logging.getLogger("eostest")
@@ -115,37 +116,37 @@ class TestRemoteCanPool(unittest.TestCase):
     def test_native_pool_deposit_episode(self):
         self.logger.info("Start test_pool_deposit", extra=self.dictLogger)
         self.client = RemoteCan(vin=self.truck.VIN)
-        self.generate_schemas()
+        self.generate_epi_schemas()
         # test schema[0]
         # self.pool = RecordPool(schema=self.schema[0], username="root", password="Newrizon123",url="mongodb://10.0.64.64:30116/", db_name="record_db", debug=True)
-        self.pool = Pool(schema=self.schema[0], db_name="test_episode_db", debug=True)
+        self.pool = Pool(schema=self.epi_schema[0], db_name="test_episode_db", coll_name="episode_coll", debug=True)
         self.logger.info("Set client", extra=self.dictLogger)
         self.get_an_episode()
-        self.logger.info("Records created.", extra=self.dictLogger)
-        self.logger.info("Start deposit records", extra=self.dictLogger)
-        for rec in self.record:
-            result = self.pool.deposit_record(rec)
-            self.logger.info("Record inserted.", extra=self.dictLogger)
-            self.assertEqual(result.acknowledged, True)
-            self.logger.info(
-                f"Pool has {self.pool.count_records()} records", extra=self.dictLogger
-            )
-            rec_inserted = self.pool.find_record_by_id(result.inserted_id)
+        self.logger.info("An Episode is created.", extra=self.dictLogger)
+        self.logger.info("Start deposit an episode", extra=self.dictLogger)
 
-            self.logger.info("record found.", extra=self.dictLogger)
-            self.assertEqual(rec_inserted["timestamp"], rec["timestamp"])
-            self.assertEqual(rec_inserted["plot"], rec["plot"])
-            self.assertEqual(rec_inserted["observation"], rec["observation"])
+        result = self.pool.deposit_episode(self.episode)
+        self.logger.info("Record inserted.", extra=self.dictLogger)
+        self.assertEqual(result.acknowledged, True)
+        self.logger.info(
+            f"Pool has {self.pool.count_episodes()} records", extra=self.dictLogger
+        )
+        epi_inserted = self.pool.find_episode_by_id(result.inserted_id)
+
+        self.logger.info("episode found.", extra=self.dictLogger)
+        self.assertEqual(epi_inserted["timestamp"], self.h_t["timestamp"])
+        self.assertEqual(epi_inserted["plot"], self.h_t["plot"])
+        self.assertEqual(epi_inserted["observation"], self.h_t["observation"])
 
         self.logger.info("End test deposit redords", extra=self.dictLogger)
 
     @unittest.skipIf(site == "internal", "skip for internal test")
     def test_native_pool_sample_episode(self):
         self.client = RemoteCan(vin=self.truck.VIN)
-        self.generate_schemas()
+        self.generate_epi_schemas()
         # self.pool = RecordPool(schema=self.schema[0], username="root", password="Newrizon123",url="mongodb://10.0.64.64:30116/", db_name="record_db", debug=True)
         self.pool = Pool(
-            schema=self.schema[0], db_name="eos_db", coll_name="record_coll", debug=True
+            schema=self.epi_schema[0], db_name="eos_db", coll_name="episode_coll", debug=True
         )
         self.logger.info("Set client", extra=self.dictLogger)
 
@@ -166,10 +167,10 @@ class TestRemoteCanPool(unittest.TestCase):
     def test_native_pool_deposit_record(self):
         self.logger.info("Start test_pool_deposit", extra=self.dictLogger)
         self.client = RemoteCan(vin=self.truck.VIN)
-        self.generate_schemas()
+        self.generate_record_schemas()
         # test schema[0]
         # self.pool = RecordPool(schema=self.schema[0], username="root", password="Newrizon123",url="mongodb://10.0.64.64:30116/", db_name="record_db", debug=True)
-        self.pool = Pool(schema=self.schema[0], db_name="test_record_db", debug=True)
+        self.pool = Pool(schema=self.rec_schema[0], db_name="test_record_db",coll_name="record_coll", debug=True)
         self.logger.info("Set client", extra=self.dictLogger)
         self.get_records()
         self.logger.info("Records created.", extra=self.dictLogger)
@@ -193,10 +194,10 @@ class TestRemoteCanPool(unittest.TestCase):
     @unittest.skipIf(site == "internal", "skip for internal test")
     def test_native_pool_sample_record(self):
         self.client = RemoteCan(vin=self.truck.VIN)
-        self.generate_schemas()
+        self.generate_record_schemas()
         # self.pool = RecordPool(schema=self.schema[0], username="root", password="Newrizon123",url="mongodb://10.0.64.64:30116/", db_name="record_db", debug=True)
         self.pool = Pool(
-            schema=self.schema[0], db_name="eos_db", coll_name="record_coll", debug=True
+            schema=self.rec_schema[0], db_name="eos_db", coll_name="record_coll", debug=True
         )
         self.logger.info("Set client", extra=self.dictLogger)
 
@@ -213,9 +214,28 @@ class TestRemoteCanPool(unittest.TestCase):
         self.logger.info("done test_pool_sample of size 24.", extra=self.dictLogger)
         self.assertEqual(len(batch_24), 24)
 
-    def generate_schemas(self):
+
+    def generate_epi_schemas(self):
         # current state
-        self.schema.append(
+        self.epi_schema.append(
+            {
+                "_id": ObjectId,
+                "timestamp": datetime,
+                "plot": {"character": str,
+                         "when": datetime,
+                         "where": str,
+                         "steps": int,
+                         "observation_dim": int,
+                         "action_row_number": int,
+                         "action_dim": int,
+                         },
+                "history": [float],
+            }
+        )
+
+    def generate_record_schemas(self):
+        # current state
+        self.rec_schema.append(
             {
                 "_id": ObjectId,
                 "timestamp": datetime,
@@ -224,7 +244,7 @@ class TestRemoteCanPool(unittest.TestCase):
             }
         )
 
-        self.schema.append(
+        self.rec_schema.append(
             {
                 "_id": ObjectId,
                 "timestamp": datetime,
@@ -247,7 +267,7 @@ class TestRemoteCanPool(unittest.TestCase):
             }
         )
 
-        self.schema.append(
+        self.rec_schema.append(
             {
                 "_id": ObjectId,
                 "timestamp": datetime,
@@ -265,23 +285,50 @@ class TestRemoteCanPool(unittest.TestCase):
     def get_an_episode(self):
 
         self.logger.info("Start get_an_episode", extra=self.dictLogger)
-        self.h_t = []
-        for i in range(5):
+        h_t_l = []
+        # action
+        k0 = 0
+        N0 = 5
+        map2d_5rows = self.vcu_calib_table_default[k0 : k0 + N0, :].reshape(-1).tolist()
+        wh1 = 0
+
+        for i in range(7):
             self.native_get()
             out = np.split(self.observation, [1, 4, 5], axis=1)  # split by empty string
             (ts, o_t0, gr_t, pow_t) = [np.squeeze(e) for e in out]
             o_t = o_t0.reshape(-1)
-            ui_sum = (
-                np.sum(np.prod(pow_t, axis=1))
-                / 3600.0
-                / self.truck.CloudSignalFrequency
-            )  # convert to Wh
-            if i > 0:
-                if i == 2:
-                    self.h_t.append(ui_sum)
+            ui_sum = np.sum(np.prod(pow_t, axis=1))
+            wh = ui_sum / 3600.0/ self.truck.CloudSignalFrequency # convert to Wh
+
+            if i % 2 == 0:
+                prev_r_t = (wh1 + wh)
+                if i > 0:
+                    if i == 2:
+                        h_t_l = [np.hstack([prev_o_t, prev_a_t, prev_r_t])]
+                    else:
+                        h_t_l.append(
+                            np.hstatck([prev_o_t, prev_a_t, prev_r_t])
+                        )
                 else:
-                    self.h_t.append(ui_sum - self.h_t[-1])
+                    timestamp0 = datetime.fromtimestamp(ts[0]/1000.0)
+            else:
+                wh1 = wh
+
+            a_t = map2d_5rows
             prev_o_t = o_t
+            prev_a_t = a_t
+            self.logger.info("End get_an_episode", extra=self.dictLogger)
+
+        self.h_t = np.array(h_t_l)
+        self.episode = {
+            "timestamp": timestamp0,
+            "plot": {
+                "character": self.truck.TruckName,
+                "when": timestamp0,
+                "where": "campus",
+            },
+            "history": self.h_t.tolist(),
+        }
 
     def get_ddpg_record(self):
         self.ddpg_schema = {
