f0349a36815df565849204021bc96c073d511e1e
binjian.xin <binjian.xin@newrizon.com>
2022-09-19 09:56:57 +0800

    [add] GitPython check

--- eos/realtime_train_infer_ddpg.py
+++ eos/realtime_train_infer_ddpg.py
@@ -44,6 +44,7 @@ from datetime import datetime
 from logging.handlers import SocketHandler
 from pathlib import Path, PurePosixPath
 from threading import Lock, Thread
+from git import Repo
 
 # from bson import ObjectId
 
@@ -117,6 +118,9 @@ class RealtimeDDPG(object):
         self.record = record
         self.path = path
 
+        self.repo = Repo(self.projroot)
+        assert self.repo.is_dirty() == False, "Repo is dirty, please commit first"
+
         if resume:
             self.dataroot = projroot.joinpath("data/" + self.path)
         else:
@@ -124,6 +128,10 @@ class RealtimeDDPG(object):
 
         self.set_logger()
         self.logger.info(f"Start Logging", extra=self.dictLogger)
+        self.logger.info(
+            f"project root: {self.projroot}, git head: {str(self.repo.head.commit)[:7]}, author: {self.repo.head.commit.author}, git message: {self.repo.head.message}",
+            extra=self.dictLogger,
+        )
 
         # validate truck id
         # assert self.truck_name in self.trucks.keys()
