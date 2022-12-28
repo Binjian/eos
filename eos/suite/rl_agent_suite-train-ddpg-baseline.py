f937a90e50d17b2952e06e9c1f62fb740bd9fafd
Binjian Xin <binjian.xin@newrizon.net>
2022-12-28 09:25:07 +0800

    [add] suite for rl_agent ddpg local train,infer

---
+++ eos/suite/rl_agent_suite-train-ddpg-baseline.py
@@ -0,0 +1,43 @@
+import datetime
+import os
+import subprocess
+import time
+
+udpfileName = (
+    os.getcwd()
+    + "/../../data/udp-pcap/rl_agent_vb-ddpg-baseline-"
+    + datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
+    + ".pcap"
+)
+# --cloud -t -p testremote -r
+portNum = 8002  # port number
+pid = os.fork()
+if pid == 0:  # copy process
+    time.sleep(1)
+    os.execlp(
+        "python",
+        "python",
+        "../rl_agent.py",
+        "--agent",
+        "ddpg",
+        "--ui",
+        "local",
+        "--resume",
+        "--path",
+        "baseline",
+        "--record_table",
+        "-v",
+        "VB7",
+        "-d",
+        "longfei",
+        "-o",
+        "mongo_local",
+    )
+
+else:
+    p = subprocess.Popen(
+        ["tcpdump", "udp", "-w", udpfileName, "-i", "lo", "port", str(portNum)],
+        stdout=subprocess.PIPE,
+    )
+    result = os.waitpid(-1, 0)
+    p.terminate()
