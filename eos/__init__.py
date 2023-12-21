5678ddb0043e8646caa0e03006fa29c4b2379081
binjian xin <binjian.xin@newrizon.com>
2023-12-21 12:40:27 +0800

    [fix] take out gpu config from __init__
    update data_io, meos-env.yml (adding jpyter contrib and black[jupiter])

--- eos/__init__.py
+++ eos/__init__.py
@@ -5,8 +5,8 @@ import tensorflow as tf
 proj_root = Path(__file__).parent.parent
 
 
-gpus = tf.config.list_physical_devices(device_type="GPU")
-tf.config.experimental.set_memory_growth(gpus[0], True)
+# gpus = tf.config.list_physical_devices(device_type="GPU")
+# tf.config.experimental.set_memory_growth(gpus[0], True)
 
 __all__ = [
     "proj_root",
